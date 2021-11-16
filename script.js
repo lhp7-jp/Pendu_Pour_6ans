2e5a79d5139fec768dcad95898751f4b6487b3ff
JeanPierre <jp1966.adoux@gmail.com>
2021-11-16 12:37:09 +0100

    Exercice Pendu pour 6ans le 16/11/2021 fin de la compréhension

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ const farmList = [
     "Coq",
     "Vache"
 ];
-const cptChar = 1;
+let cptChar = 0;
+let nbLife = 10;
 
 alphabet.forEach(Element => {
     let myLetter = Element;
@@ -23,26 +24,45 @@ function aleaNb(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 };
 
-    let cptChar1 = cptChar + 1;
-    let nomAnimal = farmList[aleaNb(0, farmList.length)];
-    let tabChar =  nomAnimal.split('');
-    tabChar.forEach(Element => {
-    let myLetterDisplay = Element;
-    let displayChar= `<div class="myLet border border-dark col rounded text-center col-2" id="mydiv${myLetterDisplay}">_</div>`;
-    indexLetter.innerHTML += (displayChar);
-    });
 
-function Action_Jouer(){
-console.log(Element);
+let nomAnimal = farmList[aleaNb(0, farmList.length)];
+let tabChar = nomAnimal.split('');
+tabChar.forEach(Element => {
+    let myLetterDisplay = Element;
+    let displayChar = `<div class="row myLet justify-content-center border border-dark rounded text-center col-2" id="indexLetter${myLetterDisplay}">_</div>`;
+    indexLetter.innerHTML += (displayChar);
+});
+
+function Action_Jouer() {
+    location.reload();
 };
 
 myKeyboard.addEventListener("click", (e) => {
     if (e.target.nodeName == "BUTTON") {
-        console.log(e.target.textContent);
+        let myScore = cptChar;
+        e.target.className = "m-2 btn btn-primary col-2";
+        e.target.disabled = true;
+        tabChar.forEach((Element, index) => {
+            console.log(e.target);
+            if (e.target.innertext.toLowerCase() == Element) {
+                document.getElementById(`indexLetter${index}`).innerText = Element;
+            cptChar++;
+            }
+        })
     }
 });
 
-let dennis = 2;
-document.getElementById(`indexLetter${dennis}`).innerText="Z";
+if (cptChar == tabChar.length) {
+    window.confirm("Gagné, voulez-vous rejouer?")
+};
+
+switch (nbLife) {
+    case 0:
+        confirm("C'est perdu, voulez-vous rejouer?")
+        break;
+
+    default:
+        break;
+};
 
 
